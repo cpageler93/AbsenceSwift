d6c34d6ad9c51af5e58ea0ed63a192f43d34ab3d
Christoph Pageler <christoph.pageler@me.com>
2017-11-25 21:18:26 +0100

    Add Async method to get absences

--- Sources/AbsenceSwift/AbsenceClient.swift
+++ Sources/AbsenceSwift/AbsenceClient.swift
@@ -27,8 +27,43 @@ public extension Absence {
         }
         
         public func absences(options: Options? = nil) -> QuackResult<Result> {
+            let body = bodyFrom(options: options)
+            let headers = [
+                HeaderKey("Content-Type"): "application/json",
+            ]
+            return respond(method: .post,
+                           path: "/api/v2/absences",
+                           body: body,
+                           headers: headers,
+                           model: Result.self,
+                           requestModification:
+            { request in
+                // use request modification block to append Hawk Auth header
+                return self.requestByAddingHawkAuth(request)
+            })
+        }
+        
+        public func absences(options: Options? = nil, completion: @escaping (QuackResult<Result>) -> ()) {
+            let body = bodyFrom(options: options)
+            let headers = [
+                HeaderKey("Content-Type"): "application/json",
+            ]
+            
+            respondAsync(method: .post,
+                         path: "/api/v2/absences",
+                         body: body,
+                         headers: headers,
+                         model: Result.self,
+                         requestModification:
+            { request in
+                // use request modification block to append Hawk Auth header
+                return self.requestByAddingHawkAuth(request)            
+            }, completion: completion)
+        }
+        
+        private func bodyFrom(options: Options?) -> [String: Any] {
             let options = options ?? Options.defaultOptions()
-            let body = [
+            return [
                 "skip": options.skip,
                 "limit": options.limit,
                 "filter": options.filter?.body() ?? [:],
@@ -37,27 +72,20 @@ public extension Absence {
                     "reasonId",
                     "approverId"
                 ]
-                ] as [String : Any]
-            
-            let uri = super.url.appendingPathComponent("/api/v2/absences").absoluteString
-            guard let headerResult = try? Hawk.Client.header(uri: uri,
-                                                             method: "POST",
+            ] as [String : Any]
+        }
+        
+        private func requestByAddingHawkAuth(_ request: Request) -> Request {
+            guard let headerResult = try? Hawk.Client.header(uri: super.url.appendingPathComponent(request.uri.description).absoluteString,
+                                                             method: request.method.description,
                                                              credentials: hawkCredentials,
                                                              nonce: "FOOBAR")
-                else {
-                    return QuackResult.failure(Error.unknown)
+            else {
+                return request
             }
             
-            let headers = [
-                HeaderKey("Content-Type"): "application/json",
-                HeaderKey("Authorization"): headerResult?.headerValue ?? "",
-                ]
-            
-            return respond(method: .post,
-                           path: "/api/v2/absences",
-                           body: body,
-                           headers: headers,
-                           model: Result.self)
+            request.headers[HeaderKey("Authorization")] = headerResult?.headerValue ?? ""
+            return request
         }
         
     }
